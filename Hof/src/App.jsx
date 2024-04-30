636b9143c8a3e2952170b431611f2907c1308e04
rapolurenu <pji.rapolu.renu@kalvium.community>
2024-04-30 13:30:44 +0530

    updated

--- Hof/src/App.jsx
+++ Hof/src/App.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import HigherOrderFunctions from './components/hof/HigherOrderFunctions';
+import HigherOrderFunctions from './components/HigherOrderFunctions';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <HigherOrderFunctions />
     </div>
   );
 }
